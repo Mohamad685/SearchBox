90fdd97ac247e8063c22606092ca47c8c343ca91
unknown <mohammad.fakih685@gmail.com>
2024-02-29 18:59:49 +0200

    Added the search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import React from 'react';
 import './styles.css';
+import { useState } from 'react';
+
 function App() {
+  const [searchBox,setSearchBox]=useState(' ');
+
+  const handleSearch= (e) => {
+    setSearchBox(e.target.value);
+  }
 
   return (
     <>
     <div className='search-page'>
       
       <h1>Search</h1>
-      <input className='search-input' placeholder='Write a word or phrase ... '/>
+      <input type='text' value={searchBox} onChange={handleSearch} className='search-input' placeholder='Write a word or phrase ... '/>
+      <div> 
+        {articles.map((article, index)=> (
+          <p key={index}>text</p>
+        ))}
+      </div>
     </div>
     
     </>
