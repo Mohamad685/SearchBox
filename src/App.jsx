cb68a71047dc3bc85f4e0ac51ab5b91ce376a706
unknown <mohammad.fakih685@gmail.com>
2024-02-29 18:41:29 +0200

    Added the title and the input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-
+import './styles.css';
 function App() {
 
   return (
-    <></>
+    <>
+    <div className='search-page'>
+      
+      <h1>Search</h1>
+      <input className='search-input' placeholder='Write a word or phrase ... '/>
+    </div>
+    
+    </>
   )
 }
 
